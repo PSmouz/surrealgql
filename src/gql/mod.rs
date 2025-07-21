9c1c95ce1377ce36a8921292fb8f2da82ebaf5a3
psmouz <49523407+PSmouz@users.noreply.github.com>
2025-07-21 19:00:31 +0200

    feat(gql): added graphiql service

--- src/gql/mod.rs
+++ src/gql/mod.rs
@@ -1,23 +1,24 @@
 use std::{
-	convert::Infallible,
-	sync::Arc,
-	task::{Context, Poll},
-	time::Duration,
+    convert::Infallible,
+    sync::Arc,
+    task::{Context, Poll},
+    time::Duration,
 };
 
 use async_graphql::{
-	http::{create_multipart_mixed_stream, is_accept_multipart_mixed},
-	Executor, ParseRequestError,
+    http::{create_multipart_mixed_stream, is_accept_multipart_mixed},
+    Executor, ParseRequestError,
 };
 use async_graphql_axum::{
-	rejection::GraphQLRejection, GraphQLBatchRequest, GraphQLRequest, GraphQLResponse,
+    rejection::GraphQLRejection, GraphQLBatchRequest, GraphQLRequest, GraphQLResponse,
 };
+use axum::response::Html;
 use axum::{
-	body::{Body, HttpBody},
-	extract::FromRequest,
-	http::{Request as HttpRequest, Response as HttpResponse},
-	response::IntoResponse,
-	BoxError,
+    body::{Body, HttpBody},
+    extract::FromRequest,
+    http::{Request as HttpRequest, Response as HttpResponse},
+    response::IntoResponse,
+    BoxError,
 };
 use bytes::Bytes;
 use futures_util::{future::BoxFuture, StreamExt};
@@ -33,111 +34,269 @@ use crate::err::Error as SurrealError;
 /// A GraphQL service.
 #[derive(Clone)]
 pub struct GraphQL<I: Invalidator> {
-	cache: SchemaCache<I>,
-	// datastore: Arc<Datastore>,
+    cache: SchemaCache<I>,
+    // datastore: Arc<Datastore>,
 }
 
 impl<I: Invalidator> GraphQL<I> {
-	/// Create a GraphQL handler.
-	pub fn new(invalidator: I, datastore: Arc<Datastore>) -> Self {
-		let _ = invalidator;
-		GraphQL {
-			cache: SchemaCache::new(datastore),
-			// datastore,
-		}
-	}
+    /// Create a GraphQL handler.
+    pub fn new(invalidator: I, datastore: Arc<Datastore>) -> Self {
+        let _ = invalidator;
+        GraphQL {
+            cache: SchemaCache::new(datastore),
+            // datastore,
+        }
+    }
 }
 
 impl<B, I> Service<HttpRequest<B>> for GraphQL<I>
 where
-	B: HttpBody<Data = Bytes> + Send + 'static,
-	B::Data: Into<Bytes>,
-	B::Error: Into<BoxError>,
-	I: Invalidator,
+    B: HttpBody<Data=Bytes> + Send + 'static,
+    B::Data: Into<Bytes>,
+    B::Error: Into<BoxError>,
+    I: Invalidator,
 {
-	type Response = HttpResponse<Body>;
-	type Error = Infallible;
-	type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+    type Response = HttpResponse<Body>;
+    type Error = Infallible;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
 
-	fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-		Poll::Ready(Ok(()))
-	}
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
 
-	fn call(&mut self, req: HttpRequest<B>) -> Self::Future {
-		let cache = self.cache.clone();
-		let req = req.map(Body::new);
+    fn call(&mut self, req: HttpRequest<B>) -> Self::Future {
+        let cache = self.cache.clone();
+        let req = req.map(Body::new);
 
-		Box::pin(async move {
-			// Check if capabilities allow querying the requested HTTP route
-			if !cache.datastore.allows_http_route(&RouteTarget::GraphQL) {
-				warn!(
+        Box::pin(async move {
+            // Check if capabilities allow querying the requested HTTP route
+            if !cache.datastore.allows_http_route(&RouteTarget::GraphQL) {
+                warn!(
 					"Capabilities denied HTTP route request attempt, target: '{}'",
 					&RouteTarget::GraphQL
 				);
-				return Ok(
-					SurrealError::ForbiddenRoute(RouteTarget::GraphQL.to_string()).into_response()
-				);
-			}
+                return Ok(
+                    SurrealError::ForbiddenRoute(RouteTarget::GraphQL.to_string()).into_response()
+                );
+            }
 
-			let session =
-				req.extensions().get::<Session>().expect("session extractor should always succeed");
+            let session =
+                req.extensions().get::<Session>().expect("session extractor should always succeed");
 
-			let Some(_ns) = session.ns.as_ref() else {
-				return Ok(to_rejection(resolver_error("No namespace specified")).into_response());
-			};
-			let Some(_db) = session.db.as_ref() else {
-				return Ok(to_rejection(resolver_error("No database specified")).into_response());
-			};
+            let Some(_ns) = session.ns.as_ref() else {
+                return Ok(to_rejection(resolver_error("No namespace specified")).into_response());
+            };
+            let Some(_db) = session.db.as_ref() else {
+                return Ok(to_rejection(resolver_error("No database specified")).into_response());
+            };
 
-			#[cfg(debug_assertions)]
-			{
-				let state = req
-					.extensions()
-					.get::<crate::net::AppState>()
-					.expect("state extractor should always succeed");
-				debug_assert!(Arc::ptr_eq(&state.datastore, &cache.datastore));
-			}
+            #[cfg(debug_assertions)]
+            {
+                let state = req
+                    .extensions()
+                    .get::<crate::net::AppState>()
+                    .expect("state extractor should always succeed");
+                debug_assert!(Arc::ptr_eq(&state.datastore, &cache.datastore));
+            }
 
-			let executor = match cache.get_schema(session).await {
-				Ok(e) => e,
-				Err(e) => {
-					info!(?e, "error generating schema");
-					return Ok(to_rejection(e).into_response());
-				}
-			};
-			let is_accept_multipart_mixed = req
-				.headers()
-				.get("accept")
-				.and_then(|value| value.to_str().ok())
-				.map(is_accept_multipart_mixed)
-				.unwrap_or_default();
+            let executor = match cache.get_schema(session).await {
+                Ok(e) => e,
+                Err(e) => {
+                    info!(?e, "error generating schema");
+                    return Ok(to_rejection(e).into_response());
+                }
+            };
+            let is_accept_multipart_mixed = req
+                .headers()
+                .get("accept")
+                .and_then(|value| value.to_str().ok())
+                .map(is_accept_multipart_mixed)
+                .unwrap_or_default();
 
-			if is_accept_multipart_mixed {
-				let req = match GraphQLRequest::<GraphQLRejection>::from_request(req, &()).await {
-					Ok(req) => req,
-					Err(err) => return Ok(err.into_response()),
-				};
-				let stream = Executor::execute_stream(&executor, req.0, None);
-				let body = Body::from_stream(
-					create_multipart_mixed_stream(stream, Duration::from_secs(30))
-						.map(Ok::<_, std::io::Error>),
-				);
-				Ok(HttpResponse::builder()
-					.header("content-type", "multipart/mixed; boundary=graphql")
-					.body(body)
-					.expect("BUG: invalid response"))
-			} else {
-				let req =
-					match GraphQLBatchRequest::<GraphQLRejection>::from_request(req, &()).await {
-						Ok(req) => req,
-						Err(err) => return Ok(err.into_response()),
-					};
-				Ok(GraphQLResponse(executor.execute_batch(req.0).await).into_response())
-			}
-		})
-	}
+            if is_accept_multipart_mixed {
+                let req = match GraphQLRequest::<GraphQLRejection>::from_request(req, &()).await {
+                    Ok(req) => req,
+                    Err(err) => return Ok(err.into_response()),
+                };
+                let stream = Executor::execute_stream(&executor, req.0, None);
+                let body = Body::from_stream(
+                    create_multipart_mixed_stream(stream, Duration::from_secs(30))
+                        .map(Ok::<_, std::io::Error>),
+                );
+                Ok(HttpResponse::builder()
+                    .header("content-type", "multipart/mixed; boundary=graphql")
+                    .body(body)
+                    .expect("BUG: invalid response"))
+            } else {
+                let req =
+                    match GraphQLBatchRequest::<GraphQLRejection>::from_request(req, &()).await {
+                        Ok(req) => req,
+                        Err(err) => return Ok(err.into_response()),
+                    };
+                Ok(GraphQLResponse(executor.execute_batch(req.0).await).into_response())
+            }
+        })
+    }
 }
 
 fn to_rejection(err: impl std::error::Error + Send + Sync + 'static) -> GraphQLRejection {
-	GraphQLRejection(ParseRequestError::InvalidRequest(Box::new(err)))
+    GraphQLRejection(ParseRequestError::InvalidRequest(Box::new(err)))
+}
+
+#[derive(Clone)]
+pub struct GraphiQL;
+
+impl GraphiQL {
+    pub fn new() -> Self {
+        GraphiQL
+    }
+}
+
+impl<B> Service<HttpRequest<B>> for GraphiQL
+where
+    B: HttpBody<Data=Bytes> + Send + 'static,
+    B::Data: Into<Bytes>,
+    B::Error: Into<BoxError>,
+{
+    type Response = HttpResponse<Body>;
+    type Error = Infallible;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: HttpRequest<B>) -> Self::Future {
+        let req = req.map(Body::new);
+
+        let path = req.uri().path().to_owned();
+
+        Box::pin(async move {
+            // This is a simple way to parse. A real-world app might use a more robust router library
+            // or pass params via extensions, but for this use case, splitting is clear.
+            // let parts: Vec<&str> = path.split('/').collect();
+            // let (ns, db) = if parts.len() >= 4 {
+            // 	(parts[2].to_string(), parts[3].to_string())
+            // } else {
+            // 	// Default values if the path is not as expected
+            // 	("test".to_string(), "test".to_string())
+            // };
+
+            let html = Html(
+                r#"
+				<!--
+				 *  Copyright (c) 2025 GraphQL Contributors
+				 *  All rights reserved.
+				 *
+				 *  This source code is licensed under the license found in the
+				 *  LICENSE file in the root directory of this source tree.
+				-->
+				<!doctype html>
+				<html lang="en">
+				  <head>
+					<meta charset="UTF-8" />
+					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+					<title>SurrealDB GraphiQL</title>
+					<style>
+					  body {
+						margin: 0;
+					  }
+
+					  #graphiql {
+						height: 100dvh;
+					  }
+
+					  .loading {
+						height: 100%;
+						display: flex;
+						align-items: center;
+						justify-content: center;
+						font-size: 4rem;
+					  }
+					</style>
+					<link rel="stylesheet" href="https://esm.sh/graphiql/dist/style.css" />
+					<link
+					  rel="stylesheet"
+					  href="https://esm.sh/@graphiql/plugin-explorer/dist/style.css"
+					/>
+					<!--
+					 * Note:
+					 * The ?standalone flag bundles the module along with all of its `dependencies`, excluding `peerDependencies`, into a single JavaScript file.
+					 * `@emotion/is-prop-valid` is a shim to remove the console error ` module "@emotion /is-prop-valid" not found`. Upstream issue: https://github.com/motiondivision/motion/issues/3126
+					-->
+					<script type="importmap">
+					  {
+						"imports": {
+						  "react": "https://esm.sh/react@19.1.0",
+						  "react/": "https://esm.sh/react@19.1.0/",
+
+						  "react-dom": "https://esm.sh/react-dom@19.1.0",
+						  "react-dom/": "https://esm.sh/react-dom@19.1.0/",
+
+						  "graphiql": "https://esm.sh/graphiql?standalone&external=react,react-dom,@graphiql/react,graphql",
+						  "graphiql/": "https://esm.sh/graphiql/",
+						  "@graphiql/plugin-explorer": "https://esm.sh/@graphiql/plugin-explorer?standalone&external=react,@graphiql/react,graphql",
+						  "@graphiql/react": "https://esm.sh/@graphiql/react?standalone&external=react,react-dom,graphql,@graphiql/toolkit,@emotion/is-prop-valid",
+
+						  "@graphiql/toolkit": "https://esm.sh/@graphiql/toolkit?standalone&external=graphql",
+						  "graphql": "https://esm.sh/graphql@16.11.0",
+						  "@emotion/is-prop-valid": "data:text/javascript,"
+						}
+					  }
+					</script>
+					<script type="module">
+					  import React from 'react';
+					  import ReactDOM from 'react-dom/client';
+					  import { GraphiQL, HISTORY_PLUGIN } from 'graphiql';
+					  import { createGraphiQLFetcher } from '@graphiql/toolkit';
+					  import { explorerPlugin } from '@graphiql/plugin-explorer';
+					  import 'graphiql/setup-workers/esm.sh';
+
+					  const customFetch = (url, opts = {}) => {
+						return fetch(url, {...opts, credentials: 'same-origin'})
+					  };
+
+					  const createUrl = (endpoint, subscription = false) => {
+						const url = new URL(endpoint, window.location.origin);
+						if (subscription) {
+						  url.protocol = url.protocol === 'https:' ? 'wss:' : 'ws:';
+						}
+						return url.toString();
+					  };
+
+					  const fetcher = createGraphiQLFetcher({
+						url: createUrl('/graphql'), // The POST endpoint TODO(psmouz): make dynamic
+						fetch: customFetch,
+						// TODO(psmouz): make dynamic
+						headers: {
+						  'surreal-ns': 'test','surreal-db': 'test','Authorization': 'Basic cm9vdDpyb290',
+						},
+					  });
+					  const plugins = [HISTORY_PLUGIN, explorerPlugin()];
+
+					  function App() {
+						return React.createElement(GraphiQL, {
+						  fetcher,
+						  plugins,
+						  defaultEditorToolsVisibility: true,
+						});
+					  }
+
+					  const container = document.getElementById('graphiql');
+					  const root = ReactDOM.createRoot(container);
+					  root.render(React.createElement(App));
+					</script>
+				  </head>
+				  <body>
+					<div id="graphiql">
+					  <div class="loading">Loading…</div>
+					</div>
+				  </body>
+				</html>
+				"#
+            );
+
+            Ok(html.into_response())
+        })
+    }
 }
